a18b83dcb5236e9529d50fb14c7db816bf50f08f
Lana <alana.rayanne@gmail.com>
2025-10-22 17:23:17 -0300

    Pratica 2 - Parte 1

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,39 +1,18 @@
 package com.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.weatherapp.ui.theme.WeatherAppTheme
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.remember
+import com.weatherapp.ui.HomePage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,29 +46,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalActivity.current as Activity
-    val scrollState = rememberScrollState()
-
-    Column(
-        modifier = modifier.padding(16.dp).fillMaxSize().verticalScroll(scrollState),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = CenterHorizontally,
-    ) {
-        Text(
-            text = "Bem-vindo/a!",
-            fontSize = 24.sp
-        )
-
-        Row(modifier = modifier) {
-
-            Spacer(modifier = modifier.size(24.dp))
-            Button(
-                onClick = { activity.finish() },
-            ) {
-                Text("Sair")
-            }
-        }
-    }
-}

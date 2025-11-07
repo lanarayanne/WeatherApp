05df5a43861e1b6652be42c1b1103ce2e412d214
Lana <alana.rayanne@gmail.com>
2025-11-07 17:24:36 -0300

    Pratica 4 - Parte 2

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.weatherapp.model.MainViewModel
 import com.weatherapp.ui.CityDialog
@@ -33,6 +36,8 @@ import com.weatherapp.ui.HomePage
 import com.weatherapp.ui.nav.BottomNavBar
 import com.weatherapp.ui.nav.BottomNavItem
 import com.weatherapp.ui.nav.MainNavHost
+import com.weatherapp.ui.nav.Route
+import androidx.navigation.NavDestination.Companion.hasRoute
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -43,6 +48,10 @@ class MainActivity : ComponentActivity() {
             var showDialog by remember { mutableStateOf(false) }
             val viewModel : MainViewModel by viewModels()
             val navController = rememberNavController()
+            val currentRoute = navController.currentBackStackEntryAsState()
+            val showButton = currentRoute.value?.destination?.hasRoute(Route.List::class) == true
+            val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(), onResult = {})
+
             WeatherAppTheme {
                 if(showDialog) CityDialog(
                     onDismiss = { showDialog = false},
@@ -82,13 +91,16 @@ class MainActivity : ComponentActivity() {
                     },
 
                     floatingActionButton = {
-
-                        FloatingActionButton(onClick = { showDialog = true }) {
-                            Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                        if (showButton) {
+                            FloatingActionButton(onClick = { showDialog = true }) {
+                                Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                            }
                         }
                     }
+
                 ) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
+                        launcher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
                         MainNavHost(navController = navController, viewModel)
                     }
                 }
--- app/src/main/java/com/weatherapp/model/City.kt
+++ app/src/main/java/com/weatherapp/model/City.kt
@@ -1,8 +1,9 @@
 package com.weatherapp.model
+import com.google.android.gms.maps.model.LatLng
 
 class City (
     val name : String,
     val weather: String? = null,
-    val location : String? = null
+    val location: LatLng? = null
 ){
 }
